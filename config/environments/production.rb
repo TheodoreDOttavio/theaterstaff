43b8925920820a8f2fc9c35c7d498b2dcba7115c
theodoredottavio <teddottavio@yahoo.com>
2015-11-21 02:01:57 -0500

    more action mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'theaterstaff.herokuapp.com' }
+  #config.action_mailer.default_url_options = { :host => 'theaterstaff.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
@@ -74,8 +74,6 @@ Rails.application.configure do
     authentication: "login",
     enable_starttls_auto: true
   }
-  #Old working verios differences:
-  #:user_name            => "teddottavio"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,13 @@
+ActionMailer::Base.smtp_settings = {
+    adress:               "smtp.gmail.com",
+    port:                 587,
+    domain:               "gmail.com",
+    user_name:            "teddottavio@gmail.com",
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       :login,
+    enable_starttls_auto: true
+}
+
+ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+
+ActionMailer::Base.raise_delivery_errors = true
