f11d46879e86966446ca0dbaee63a4ea2ace02bd
theodoredottavio <teddottavio@yahoo.com>
2015-11-21 02:12:35 -0500

    gmail settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.smtp_settings = {
-    adress:               "smtp.gmail.com",
+    address:               "smtp.gmail.com",
     port:                 587,
     domain:               "gmail.com",
     user_name:            "teddottavio@gmail.com",
