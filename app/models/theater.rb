5de38ab7727ccfd8f37bda4b1c3e5d66a4d48c01
TheodoreDOttavio <teddottavio@yahoo.com>
2015-11-28 21:38:09 -0500

    moving functions from controllers to models didnt work

--- app/models/theater.rb
+++ app/models/theater.rb
@@ -9,15 +9,4 @@ class Theater < ActiveRecord::Base
   validates :commentslock, length: { maximum: 4 }
   
   delegate :name, :closeing, :to => :performance, :prefix => true
-  
-  def companylist
-    companies = []
-    companies.push(["Independent","Independent"])
-    companies.push(["Disney","Disney"])
-    companies.push(["Nederlander","Nederlander"])
-    companies.push(["Jujamcyn","Jujamcyn"])
-    companies.push(["Schubert","Schubert"])
-    companies.push(["Roundabout","Roundabout"])
-    return companies
-  end
 end
