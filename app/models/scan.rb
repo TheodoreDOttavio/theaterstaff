afe2ce8e761b07e4aeef5e4a9d9311bd327f959d
theodoredottavio <teddottavio@yahoo.com>
2016-03-30 14:26:39 -0400

    showing ss log count in data entry

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -1,6 +1,7 @@
 class Scan < ActiveRecord::Base
   #Scopes for Paperwork and Reports
   scope :scanscount, ->(mystart) { where(monday: mystart, specialservices: true).uniq.pluck(:performance_id).count }
+  scope :ssscanscount, ->(mystart) { where(monday: mystart, specialservices: false).uniq.pluck(:performance_id).count }
   
   scope :anydate, ->(myday) { where(monday: weekstart(myday)) }
 
